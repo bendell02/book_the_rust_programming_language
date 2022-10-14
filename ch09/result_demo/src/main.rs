5e5bf7188e31a1bb7d4519ec6c62cad8dc3c30ae
pengyong <yong.peng@deptrum.com>
2022-10-14 22:21:21 +0800

    update ch09_result_demo

--- ch09/result_demo/src/main.rs
+++ ch09/result_demo/src/main.rs
@@ -42,10 +42,105 @@
 //     let f = File::open("hello.txt").unwrap();
 // }
 
-// 3. version 4
+// // 3. version 4
+// use std::fs::File;
+
+// fn main() {
+//     let f = File::open("hello.txt").expect("Failed to open hello.txt");
+// }
+
+
+// // 4. return Result to caller
+// use std::io;
+// use std::io::Read;
+// use std::fs::File;
+
+// fn read_username_from_file() -> Result<String,  io::Error> {
+//     let f = File::open("hello.txt");
+
+//     let mut f = match f {
+//         Ok(file) => file,
+//         Err(e) => return Err(e),
+//     };
+
+//     let mut s = String::new();
+
+//     match f.read_to_string(&mut s) {
+//         Ok(_) => Ok(s),
+//         Err(e) => Err(e),
+//     }
+// }
+
+// fn main() {
+//     let rlt = read_username_from_file();
+
+//     println!("{:?}", rlt);
+// }
+
+
+// // 5. return Result to caller with ?
+// use std::io;
+// use std::io::Read;
+// use std::fs::File;
+
+// fn read_username_from_file() -> Result<String,  io::Error> {
+//     let mut f = File::open("hello.txt")?;
+
+//     let mut s = String::new();
+
+//     f.read_to_string(&mut s)?;
+//     Ok(s)
+// }
+
+// fn main() {
+//     let rlt = read_username_from_file();
+
+//     println!("{:?}", rlt);
+// }
+
+
+// // 6. return Result to caller with ?
+// use std::io;
+// use std::io::Read;
+// use std::fs::File;
+
+// fn read_username_from_file() -> Result<String,  io::Error> {
+//     let mut s = String::new();
+
+//     File::open("hello.txt")?.read_to_string(&mut s)?;
+
+//     Ok(s)
+// }
+
+// fn main() {
+//     let rlt = read_username_from_file();
+
+//     println!("{:?}", rlt);
+// }
+
+
+// // 7. simple presentation
+// use std::io;
+// use std::fs;
+
+// fn read_username_from_file() -> Result<String,  io::Error> {
+//     fs::read_to_string("hello.txt")
+// }
+
+// fn main() {
+//     let rlt = read_username_from_file();
+
+//     println!("{:?}", rlt);
+// }
+
+
+// 8.()
+use std::error::Error;
 use std::fs::File;
 
-fn main() {
-    let f = File::open("hello.txt").expect("Failed to open hello.txt");
+fn main() ->Result<(), Box<dyn Error>> {
+    let f = File::open("hello.txt")?;
+
+    Ok(())
 }
 
