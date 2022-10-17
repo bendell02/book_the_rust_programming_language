c830562ad36cce740eca428a1ec08b72a14a1736
pengyong <yong.peng@deptrum.com>
2022-10-17 21:03:31 +0800

    add ch10_trait_demo

---
+++ ch10/trait_demo/src/main.rs
@@ -0,0 +1,22 @@
+
+use trait_demo::*;
+
+fn main() {
+    let tweet = Tweet {
+        username: String::from("horse_ebooks"),
+        content: String::from("of course, as you probably already know, people"),
+        reply: false,
+        retweet: false,
+    };
+
+    println!("1 new tweet: {}", tweet.summarize());
+
+    let article = NewsArticle {
+        headline: String::from("Penguins win the Stanley Cup Championship!"),
+        location: String::from("Pittsburgb"),
+        author: String::from("Iceburgh"),
+        content: String::from("The Pittsburgh Penguins once again are the best hockey tem in the NHL."),
+    };
+
+    println!("New article available! {}", article.summarize());
+}
