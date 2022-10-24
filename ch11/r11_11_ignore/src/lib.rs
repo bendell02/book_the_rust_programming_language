a91de5c5d4ab147f2b68a38ef2f59b79c7395edc
pengyong <yong.peng@deptrum.com>
2022-10-24 20:17:09 +0800

    add ch11_r11_10 and r11_11

---
+++ ch11/r11_11_ignore/src/lib.rs
@@ -0,0 +1,23 @@
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+
+    #[test]
+    #[ignore]
+    fn expensive_test() {
+        println!("expensive_test()");
+    }
+}
+
+// cargo test
+// cargo test -- --ignored  
