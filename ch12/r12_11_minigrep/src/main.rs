30edd079bea3cf6624e291f376e23104567dd981
pengyong <yong.peng@deptrum.com>
2022-11-10 23:41:21 +0800

    add r12_11

---
+++ ch12/r12_11_minigrep/src/main.rs
@@ -0,0 +1,27 @@
+
+use std::env;
+use std::process;
+
+use r12_11_minigrep::Config;
+
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let config  = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+
+    if let Err(e) = r12_11_minigrep::run(config) {
+        println!("Application error: {}", e);
+
+        process::exit(1);
+    }
+}
+
+
+
+// cargo run the poem.txt
+
+
